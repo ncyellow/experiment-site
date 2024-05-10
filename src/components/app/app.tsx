c5b6784f5a85c29f2e9e4b57662ca1ed0d618458
ncyellow <ncyellow@yandex.ru>
2024-05-10 22:42:32 +0300

    base settings

---
+++ src/components/app/app.tsx
@@ -0,0 +1,10 @@
+import '../../index.css';
+import styles from './app.module.css';
+
+const App = () => (
+  <div>
+    <h1>Начало</h1>
+  </div>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import App from './components/app/app';
+
+const container = document.getElementById('root') as HTMLElement;
+const root = ReactDOMClient.createRoot(container!);
+
+root.render(
+  <React.StrictMode>
+    {/* <BrowserRouter>
+      <Provider> */}
+    <App />
+    {/* </Provider>
+    </BrowserRouter> */}
+  </React.StrictMode>
+);
