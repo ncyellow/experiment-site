b5fd869a528b661d322c0ddd66e71f82ec3549b6
ncyellow <ncyellow@yandex.ru>
2024-05-10 23:06:25 +0300

    add bootstrap react

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,15 @@
 import '../../index.css';
+import { clsx } from 'clsx';
+import Container from 'react-bootstrap/Container';
+import { Button } from 'react-bootstrap';
+
 import styles from './app.module.css';
 
 const App = () => (
-  <div>
+  <Container>
     <h1>Начало</h1>
-  </div>
+    <Button>SuperButton</Button>
+  </Container>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './components/app/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
